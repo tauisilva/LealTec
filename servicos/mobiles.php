7d6dd313fa7d770a46a7208474bfa44300a627ef
Taui Silva <tauisilva@gmail.com>
2019-06-03 16:44:54 -0300

    Modal
    adicionado modal ordem de servico

--- servicos/mobiles.php
+++ servicos/mobiles.php
@@ -3,8 +3,8 @@
   aria-hidden="true" >
   <div class="modal-dialog form-dark" role="document">
     <!--Content-->
-    <div class="modal-content card card-image" style="background-image: url('img/mimix32.jpg'); border-radius: 20px;">
-      <div class="text-white rgba-stylish-strong py-5 px-5 z-depth-4" style="border-radius: 20px;">
+    <div class="modal-content card card-image" style="background-image: url('img/mimix32.jpg'); border-radius: 10px;">
+      <div class="text-white rgba-stylish-strong py-5 px-5 z-depth-4" style="border-radius: 10px;">
         <!--Header-->
         <div class="modal-header text-center pb-4">
           <h3 class="modal-title w-100 white-text font-weight-bold" id="modalServicosLabel"><a
@@ -18,16 +18,16 @@
           <!--Body-->
           <div class="row d-flex align-items-center mb-4">
             <div class="text-center mb-3 col-md-12" >
-              <button type="button" class="btn btn-outline-success btn-rounded waves-effect btn-block z-depth-1" data-toggle="modal" data-target="#modalSmartphones" style="border-radius: 20px;">Smartphone</button>
+              <button type="button" class="btn btn-outline-success btn-rounded waves-effect btn-block z-depth-1" data-toggle="modal" data-target="#modalSmartphones" style="border-radius: 10px;">Smartphone</button>
             </div>
             <div class="text-center mb-3 col-md-12" >
-              <button type="button" class="btn btn-outline-success btn-rounded waves-effect btn-block z-depth-1"  data-toggle="modal" data-target="#modalTablets"style="border-radius: 20px;">Tablets</button>
+              <button type="button" class="btn btn-outline-success btn-rounded waves-effect btn-block z-depth-1"  data-toggle="modal" data-target="#modalTablets"style="border-radius: 10px;">Tablets</button>
             </div>
           </div>
         </div>
         <div class="modal-footer">
-        <button type="button"class="btn btn-outline-danger waves-effect" style="border-radius: 20px;" data-dismiss="modal">Close</button>
-        <button class="btn btn-outline-success waves-effect z-depth-1" style="border-radius: 20px;">Novo Serviço</button>
+        <button type="button"class="btn btn-outline-danger waves-effect" style="border-radius: 10px;" data-dismiss="modal">Close</button>
+        <button class="btn btn-outline-success waves-effect z-depth-1" style="border-radius: 10px;" data-toggle="modal" data-target="#modalOrdem" data-dismiss="modal">Novo Serviço</button>
       </div>
       </div>
     </div>
@@ -40,9 +40,9 @@
 <div class="modal fade" id="modalSmartphones" tabindex="-1" role="dialog" aria-labelledby="modalSmartphonesLabel"
   aria-hidden="true">
   <div class="modal-dialog modal-fluid" role="document">
-    <div class="modal-content" style="border-radius: 20px;">
+    <div class="modal-content" style="border-radius: 10px;">
       <!--Header-->
-      <div class="modal-header" style="background-image: linear-gradient(to right, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%); border-top-right-radius:  20px; border-top-left-radius: 20px;">
+      <div class="modal-header" style="background-image: linear-gradient(to right, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%); border-top-right-radius:  10px; border-top-left-radius: 10px;">
         <h4 class="modal-title white-text" id="myModalSmartphones">Smartphones</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">×</span>
@@ -97,8 +97,8 @@
       </div>
       <!--Footer-->
       <div class="modal-footer">
-        <button type="button"class="btn btn-outline-secondary waves-effect" style="border-radius: 20px;" data-dismiss="modal" data-target="#modalServicos">voltar</button>
-        <button class="btn btn-outline-primary waves-effect" style="border-radius: 20px;">Checkout</button>
+        <button type="button"class="btn btn-outline-secondary waves-effect" style="border-radius: 10px;" data-dismiss="modal" data-target="#modalServicos">voltar</button>
+        <button class="btn btn-outline-primary waves-effect" style="border-radius: 10px;">Checkout</button>
       </div>
     </div>
   </div>
@@ -109,9 +109,9 @@
 <div class="modal fade" id="modalTablets" tabindex="-1" role="dialog" aria-labelledby="modalTabletsLabel"
   aria-hidden="true">
   <div class="modal-dialog modal-fluid" role="document">
-    <div class="modal-content" style="border-radius: 20px;">
+    <div class="modal-content" style="border-radius: 10px;">
       <!--Header-->
-      <div class="modal-header" style="background-image: linear-gradient(to right, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%); border-top-right-radius:  20px; border-top-left-radius: 20px;">
+      <div class="modal-header" style="background-image: linear-gradient(to right, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%); border-top-right-radius:  10px; border-top-left-radius: 10px;">
         <h4 class="modal-title white-text" id="myModalTablets">Tablets</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">×</span>
@@ -166,10 +166,11 @@
       </div>
       <!--Footer-->
       <div class="modal-footer">
-        <button type="button"class="btn btn-outline-secondary waves-effect" style="border-radius: 20px;" data-dismiss="modal" data-target="#modalServicos">voltar</button>
-        <button class="btn btn-outline-primary waves-effect" style="border-radius: 20px;">Checkout</button>
+        <button type="button"class="btn btn-outline-secondary waves-effect" style="border-radius: 10px;" data-dismiss="modal" data-target="#modalServicos">voltar</button>
+        <button class="btn btn-outline-primary waves-effect" style="border-radius: 10px;">Checkout</button>
       </div>
     </div>
   </div>
 </div>
 <!-- Modal: Tablets -->
+<?php include("ordem.php");?>
