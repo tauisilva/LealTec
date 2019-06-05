92c6051718977a42bae6d552477bd165aefce193
Taui Silva <tauisilva@gmail.com>
2019-06-04 23:21:50 -0300

    update outros modal

--- servicos/outros.php
+++ servicos/outros.php
@@ -18,10 +18,7 @@
           <!--Body-->
           <div class="row d-flex align-items-center mb-4">
             <div class="text-center mb-3 col-md-12" >
-              <button type="button" class="btn btn-outline-success btn-rounded waves-effect btn-block z-depth-1" data-toggle="modal" data-target="#modalSmartphones" style="border-radius: 10px;">Smartphone</button>
-            </div>
-            <div class="text-center mb-3 col-md-12" >
-              <button type="button" class="btn btn-outline-success btn-rounded waves-effect btn-block z-depth-1"  data-toggle="modal" data-target="#modalTablets"style="border-radius: 10px;">Tablets</button>
+              <button type="button" class="btn btn-outline-success btn-rounded waves-effect btn-block z-depth-1"  data-toggle="modal" data-target="#modaletc"style="border-radius: 10px;">Etc</button>
             </div>
           </div>
         </div>
@@ -36,3 +33,69 @@
 </div>
 <!-- Modal mobiles-->
 <?php include("ordem.php");?>
+<div class="modal fade" id="modaletc" tabindex="-1" role="dialog" aria-labelledby="modaletcLabel"
+  aria-hidden="true">
+  <div class="modal-dialog modal-fluid" role="document">
+    <div class="modal-content" style="border-radius: 10px;">
+      <!--Header-->
+      <div class="modal-header" style="background-image: linear-gradient(to right, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%); border-top-right-radius:  10px; border-top-left-radius: 10px;">
+        <h4 class="modal-title white-text" id="myModaletc">etc</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <!--Body-->
+      <div class="modal-body">
+
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product name</th>
+              <th>Price</th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Product 1</td>
+              <td>100$</td>
+              <td><a><i class="fas fa-times"></i></a></td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Product 2</td>
+              <td>100$</td>
+              <td><a><i class="fas fa-times"></i></a></td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Product 3</td>
+              <td>100$</td>
+              <td><a><i class="fas fa-times"></i></a></td>
+            </tr>
+            <tr>
+              <th scope="row">4</th>
+              <td>Product 4</td>
+              <td>100$</td>
+              <td><a><i class="fas fa-times"></i></a></td>
+            </tr>
+            <tr class="total">
+              <th scope="row">5</th>
+              <td>Total</td>
+              <td>400$</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+      <!--Footer-->
+      <div class="modal-footer">
+        <button type="button"class="btn btn-outline-secondary waves-effect" style="border-radius: 10px;" data-dismiss="modal" data-target="#modalOutros">voltar</button>
+        <button class="btn btn-outline-primary waves-effect" style="border-radius: 10px;">Checkout</button>
+      </div>
+    </div>
+  </div>
+</div>
